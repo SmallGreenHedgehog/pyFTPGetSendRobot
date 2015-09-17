1b62933ca32a5f88cbc6040a20bc855bee02a0e5
Jill <jill.overlord@gmail.com>
2015-09-17 12:54:33 +0300

    Добавлено обратимое преобразование паролей для хранения в закрытом виде (защита от "дурака"). Signed-off-by: Jill <jill.overlord@gmail.com>

--- jillmodule.py
+++ jillmodule.py
@@ -43,16 +43,51 @@ class JpassHider:
 
         lenthpasswd=len(passwd)
         if lenthpasswd>0:
-            i=lenthpasswd
-            arraycodepasswd=[]
-            while i>0:
-                arraycodepasswd.append(ord(passwd[i]))
+            i=lenthpasswd-1
+            codepasswd=''
+            while not(i<0): #Читаем фразу в обратную сторону и преобразовываем
+                code=ord(passwd[i])
+                newcode=4*code**2-20*code+25
+                delimcode=34
+                codepasswd+=str(newcode)+str(delimcode)
+                i-=1
 
-
-
+            i=0
+            lenthcodepasswd=len(codepasswd)
+            while i<lenthcodepasswd:
+                codeforencr=40+int(codepasswd[i])
+                encrpasswd+=chr(codeforencr)
+                i+=1
         return encrpasswd
 
     def decrypt(self, encrpasswd):
         passwd=''
+        lenthencrpasswd=len(encrpasswd)
+        i=0
+        codepasswd=''
+        while i<lenthencrpasswd:
+            code=ord(encrpasswd[i])-40
+            codepasswd+=str(code)
+            i+=1
 
+        #Распарсим строку и разобьем на числа
+        arraypasswd=[]
+        lenthcodepasswd=len(codepasswd)
+        strsym=''
+        i=0
+        while i<lenthcodepasswd:
+            if codepasswd[i:i+2]=='34':
+                arraypasswd.append(int(strsym))
+                strsym=''
+                i+=1
+            else:
+                strsym+=codepasswd[i]
+            i+=1
+
+        i=len(arraypasswd)-1
+        while not(i<0):
+            code=arraypasswd[i]
+            oldcode=int((code**0.5+5)/2)
+            passwd+=chr(oldcode)
+            i-=1
         return passwd
